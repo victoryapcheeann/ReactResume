1e9499649f3400eccc9c281b5d073ff1805e7651
Victor Yap <victoryap@Victors-MacBook-Air.local>
2017-02-27 16:42:55 +0800

    Added about page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -13,7 +13,7 @@ var Header = React.createClass({
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
-   	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
+   	           <li><a className="smoothscroll" href="#resume">Resume</a></li>
                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
                <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
